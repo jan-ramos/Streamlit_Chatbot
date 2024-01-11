b3ea31401e08ea3ce814d958569641b5266aff08
Jan Ramos <87098949+jan-ramos@users.noreply.github.com>
2024-01-11 02:02:32 -0500

    Update auth.py
    updated authentication

--- utils/auth.py
+++ utils/auth.py
@@ -5,17 +5,18 @@ def is_api_key_valid(key):
     if key == 'test':
         return True
     else:
-        try:
-             openai.api_key = key
-             response = openai.Completion.create(
-                engine="davinci",
-                prompt="This is a test.",
-                max_tokens=5
-            )
-        except:
-            return False
-        else:
-            return True
+        #try:
+        #     openai.api_key = key
+        #     response = openai.Completion.create(
+        #        engine="davinci",
+        #        prompt="This is a test.",
+        #        max_tokens=5
+        #    )
+        #except:
+         #   return False
+        #else:
+        openai.api_key = key
+        return True
 
 def creds_entered():
     if is_api_key_valid(st.session_state['password']):
